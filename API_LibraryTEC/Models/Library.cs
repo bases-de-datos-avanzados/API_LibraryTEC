c267ba7ea6542a65dc3b0a045f17873841e099ba
juanp1995 <juanp1995@gmail.com>
2019-05-19 01:01:25 -0600

    Update Library model with "idManager" field

--- API_LibraryTEC/Models/Library.cs
+++ API_LibraryTEC/Models/Library.cs
@@ -7,32 +7,9 @@ using System.Threading.Tasks;
 
 namespace API_LibraryTEC.Models
 {
-    public class Library
+
+    static class CONSTANTS_LIBRARY
     {
-        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
-        public string Id { get; set; }
-
-        [BsonElement("name")]
-        public string Name { get; set; }
-
-        [BsonElement("country")]
-        public string Country { get; set; }
-
-        [BsonElement("location")]
-        public string Location { get; set; }
-
-        [BsonElement("phone")]
-        public string Phone { get; set; }
-
-        [BsonElement("schedule")]
-        public string Schedule { get; set; }
-
-        
-
-    }
-
-
-    static class CONSTANTS_LIBRARY {
         public const string LIBRARIES_COLLECTION = "Libraries";
         public const string ID = "_id";
         public const string NAME = "name";
@@ -40,6 +17,32 @@ namespace API_LibraryTEC.Models
         public const string LOCATION = "location";
         public const string PHONE = "phone";
         public const string SCHEDULE = "schedule";
+        public const string ID_MANAGER = "idManager";
+    }
+
+    public class Library
+    {
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+        public string Id { get; set; }
+
+        [BsonElement(CONSTANTS_LIBRARY.NAME)]
+        public string Name { get; set; }
+
+        [BsonElement(CONSTANTS_LIBRARY.COUNTRY)]
+        public string Country { get; set; }
+
+        [BsonElement(CONSTANTS_LIBRARY.LOCATION)]
+        public string Location { get; set; }
+
+        [BsonElement(CONSTANTS_LIBRARY.PHONE)]
+        public string Phone { get; set; }
+
+        [BsonElement(CONSTANTS_LIBRARY.SCHEDULE)]
+        public string Schedule { get; set; }
+
+        [BsonElement(CONSTANTS_LIBRARY.ID_MANAGER)]
+        public string IdManager { get; set; } = "";
+
     }
 
 }
